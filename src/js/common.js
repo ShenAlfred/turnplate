5428c725ab0eb378b4af33ae45ea8065f215ba6e
Alfred <shenyuanchen@vchangyi.com>
2018-10-16 11:43:00 +0800

    skin

--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,48 @@
+var baseUrl = 'http://192.168.43.219:8007/v2';
+
+
+/***** 测试皮肤 */
+var theme = 'chinared';
+$("body").addClass(theme);
+// $(".bg1").attr('src', '../image/turnplate_a/skyblue/bg.png');
+// $(".broadcast-img").attr('src', '../image/turnplate_a/skyblue/msg.png');
+// $(".title").attr('src', '../image/turnplate_a/skyblue/title.png');
+// $(".left-btn").attr('src', '../image/turnplate_a/skyblue/left_btn.png');
+// $(".right-btn").attr('src', './image/turnplate_a/skyblue/right_btn.png');
+// $(".turntable_bg").attr('src', '../image/turnplate/skyblue/turntable_bg.png');
+// $(".ybd").attr('src', '../image/turnplate_a/skyblue/ybd.png');
+// $(".cd-item").css({
+// 	background: "url('../image/countdown/skyblue/cd.png') center center no-repeat",
+// 	backgroundSize: '100%'
+// });
+// $(".chou-btn").css({
+// 	background: "url('../image/trunplate/skyblue/chou_btn.png') center center no-repeat",
+// 	backgroundSize: '100%'
+// });
+// $(".turntable_bg").css({
+// 	boxShadow: '0 0 60px #2b5dce'
+// });
+// $(".chou-btn").css({
+// 	background: 'url("../image/turnplate/skyblue/chou_btn.png") center center no-repeat',
+//     backgroundSize: '100%'
+// });
+
+
+
 function rnd(n, m){
 	var random = Math.floor(Math.random()*(m-n+1)+n);
 	return random;
 }
+
+function GetRequest() {   
+	var url = location.search; //获取url中"?"符后的字串   
+	var theRequest = new Object();   
+	if (url.indexOf("?") != -1) {   
+	   var str = url.substr(1);   
+	   strs = str.split("&");   
+	   for(var i = 0; i < strs.length; i ++) {   
+		  theRequest[strs[i].split("=")[0]]=unescape(strs[i].split("=")[1]);   
+	   }   
+	}   
+	return theRequest;   
+ }   
--- src/js/turntable.js
+++ src/js/turntable.js
@@ -7,6 +7,8 @@ function turntable() {
     this.small_textRadius = 300;
     this.bRotate = false;
     this.scale = 6;
+    this.colorsGroup = ['#ff6026', '#ffffff'];
+    this.mainColor = '#ff6026';
 }
 
 //16
@@ -32,9 +34,7 @@ turntable.prototype = {
         $canvas.width = $canvas.width * this.scale;
         $canvas.height = $canvas.height * this.scale;
 
-        this.handleRestarauntsData();
         this.drawTurntableWheel(ctx);
-
         var custom_angle = 0;
         if(this.restaraunts.length == 6) {
             custom_angle = 0;
@@ -42,6 +42,10 @@ turntable.prototype = {
             custom_angle = 68;
         }else if(this.restaraunts.length == 10) {
             custom_angle = 35;
+        }else if(this.restaraunts.length == 12) {
+            custom_angle = 15;
+        }else if(this.restaraunts.length == 16) {
+            custom_angle = 10;
         }else {
             custom_angle = 0;
         }
@@ -51,7 +55,8 @@ turntable.prototype = {
     },
     rotateFn: function (item, txt){
         var that = this;
-        var angles = item * (360 / this.restaraunts.length) - (360 / (this.restaraunts.length*2));
+        var angles = item * (360 / that.restaraunts.length) - (360 / (that.restaraunts.length*2));
+        console.log(angles)
         if(angles<270){
             angles = 270 - angles; 
         }else{
@@ -61,25 +66,12 @@ turntable.prototype = {
         $("#canvas").rotate({
             angle: 0,
             animateTo:angles+7200,
-            duration: 5000,
+            duration: 8000,
             callback:function (){
                 console.log(txt);
                 that.bRotate = !that.bRotate;
-                console.log(that.bRotate)
             }
         });
-    },
-    //处理奖品数据(分割两个数组)
-    handleRestarauntsData: function() {
-        for(var k=0; k<this.restaraunts.length; k++) {
-            if(k%2 == 0) {
-                this.big_circle_data.push(this.restaraunts[k]);
-                this.small_circle_data.push("");
-            }else {
-                this.big_circle_data.push("");
-                this.small_circle_data.push(this.restaraunts[k]);
-            }
-        }
     },
     //画转盘
     drawTurntableWheel: function(ctx) {
@@ -91,116 +83,49 @@ turntable.prototype = {
         
 
         //圆的容器（包含小圆和大圆）
-        ctx.fillStyle = "transparent";
+        ctx.fillStyle = 'rgba(255,255,255,.5)';
         ctx.beginPath();
-        ctx.arc(center,center,center-15*this.scale,0,2*Math.PI);
+        ctx.arc(center,center,center-12*this.scale,0,2*Math.PI);
         ctx.stroke();
         ctx.fill();
         
-        //小圆的渐变色
-        var small_grd=ctx.createRadialGradient(center,center,0,center,center,center-25);
-        small_grd.addColorStop(0,"#fff");
-        small_grd.addColorStop(0.7,"#fff");
-        small_grd.addColorStop(1,"#fff2bc");
-        ctx.fillStyle = small_grd;
         //小圆
-        for(var i=0;i<that.small_circle_data.length; i++) {
-            if(that.small_circle_data[i]) {
-                var angle = 0 + i * arc;
-                ctx.beginPath();
-                ctx.arc(center, center, center-15*this.scale, angle, angle + arc, false);
-                ctx.arc(center, center, 0, angle + arc, angle, true);
-                ctx.stroke();
-                ctx.fill();
-                ctx.save(); 
-                ctx.fillStyle = "#ff5122";
-                if(this.restaraunts.length == 6){
-                    ctx.font=16*this.scale/1.5 + "px Calibri";
-                }else if(this.restaraunts.length >=6 && this.restaraunts.length <=8) {
-                    ctx.font=14*this.scale/1.5 + "px Calibri";
-                }else if(this.restaraunts.length >=8 && this.restaraunts.length <=10) {
-                    ctx.font=12*this.scale/1.5 + "px Calibri";
-                }else {
-                    ctx.font=12*this.scale/1.5 + "px Calibri";
-                }
-                var text = that.small_circle_data[i];
-                var line_height = 17;
-                if(this.restaraunts.length == 6){
-                    ctx.translate(center + Math.cos(angle + arc / 2) * this.big_textRadius, center + Math.sin(angle + arc / 2) * this.big_textRadius);
-                    ctx.rotate(angle + arc / 2 + Math.PI / 2);
-                }else if(this.restaraunts.length >=6 && this.restaraunts.length <=8) {
-                    ctx.translate(center + Math.cos(angle + arc / 1.7) * this.big_textRadius, center + Math.sin(angle + arc / 1.8) * this.big_textRadius);
-                    ctx.rotate(angle + arc/2);
-                }else if(this.restaraunts.length >=8 && this.restaraunts.length <=10) {
-                    ctx.translate(center + Math.cos(angle + arc / 1.5) * this.big_textRadius, center + Math.sin(angle + arc / 1.8) * this.big_textRadius);
-                    ctx.rotate(angle + arc/2);
-                }else {
-                    ctx.translate(center + Math.cos(angle + arc / 1.5) * this.big_textRadius, center + Math.sin(angle + arc / 1.8) * this.big_textRadius);
-                    ctx.rotate(angle + arc/2);
-                }
-                ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
-                ctx.restore();
+        for(var i=0;i<this.restaraunts.length; i++) {
+            var angle = 0 + i * arc;
+            ctx.fillStyle = this.colorsGroup[i%2];
+            ctx.beginPath();
+            ctx.arc(center, center, center-18*this.scale, angle, angle + arc, false);
+            ctx.arc(center, center, 0, angle + arc, angle, true);
+            ctx.stroke();
+            ctx.fill();
+            ctx.save(); 
+            ctx.fillStyle = this.colorsGroup[ (i%2)? (i%2)-1 : (i%2)+1 ];
+            if(this.restaraunts.length == 6){
+                ctx.font=16*this.scale/1.5 + "px Calibri";
+            }else if(this.restaraunts.length >=6 && this.restaraunts.length <=8) {
+                ctx.font=14*this.scale/1.5 + "px Calibri";
+            }else if(this.restaraunts.length >=8 && this.restaraunts.length <=10) {
+                ctx.font=12*this.scale/1.5 + "px Calibri";
+            }else {
+                ctx.font=12*this.scale/1.5 + "px Calibri";
             }
-        }
-
-        //大圆的渐变色
-        var big_grd=ctx.createRadialGradient(center,center,0,center,center,center-18);
-        big_grd.addColorStop(0,"#FF7A4B");
-        big_grd.addColorStop(0.5,"#FFBA00");
-        big_grd.addColorStop(1,"#FFD4B4");
-        ctx.fillStyle = big_grd;
-
-        //大圆阴影
-        ctx.shadowOffsetX = 0; // 阴影Y轴偏移
-        ctx.shadowOffsetY = 0; // 阴影X轴偏移
-        ctx.shadowBlur = 30; // 模糊尺寸
-        ctx.shadowColor = 'rgba(0, 0, 0, 0.5)'; // 颜色
-        // //大圆
-        for(var i=0;i<that.big_circle_data.length; i++) {
-            if(that.big_circle_data[i]) {
-                var angle = 0 + i * arc;
-                ctx.beginPath();
-                ctx.arc(center, center, center-12*this.scale, angle, angle + arc, false);
-                ctx.arc(center,center, 0, angle + arc, angle, true);
-                ctx.stroke();
-                ctx.fill();
-                ctx.save(); 
-                if(i%2 == 0) {
-                    ctx.fillStyle = "#fff";
-                    if(this.restaraunts.length == 6){
-                        ctx.font=16*this.scale/1.5 + "px Calibri";
-                    }else if(this.restaraunts.length >=6 && this.restaraunts.length <=8) {
-                        ctx.font=14*this.scale/1.5 + "px Calibri";
-                    }else if(this.restaraunts.length >=8 && this.restaraunts.length <=10) {
-                        ctx.font=12*this.scale/1.5 + "px Calibri";
-                    }else {
-                        ctx.font=12*this.scale/1.5 + "px Calibri";
-                    }
-                    ctx.shadowOffsetX = 0; 
-                    ctx.shadowOffsetY = 0; 
-                    ctx.shadowBlur = 0; 
-                    ctx.shadowColor = 'rgba(255, 255, 255, 0)'; 
-                    var text = that.big_circle_data[i];
-                    var line_height = 17;
-                    if(this.restaraunts.length == 6){
-                        ctx.translate(center + Math.cos(angle + arc / 2) * this.big_textRadius, center + Math.sin(angle + arc / 2) * this.big_textRadius);
-                        ctx.rotate(angle + arc / 2 + Math.PI / 2);
-                    }else if(this.restaraunts.length >=6 && this.restaraunts.length <=8) {
-                        ctx.translate(center + Math.cos(angle + arc / 1.7) * this.big_textRadius, center + Math.sin(angle + arc / 1.8) * this.big_textRadius);
-                        ctx.rotate(angle + arc/2);
-                    }else if(this.restaraunts.length >=8 && this.restaraunts.length <=10) {
-                        ctx.translate(center + Math.cos(angle + arc / 1.5) * this.big_textRadius, center + Math.sin(angle + arc / 1.8) * this.big_textRadius);
-                        ctx.rotate(angle + arc/2);
-                    }else {
-                        ctx.translate(center + Math.cos(angle + arc / 1.5) * this.big_textRadius, center + Math.sin(angle + arc / 1.8) * this.big_textRadius);
-                        ctx.rotate(angle + arc/2);
-                    }
-                    // ctx.translate(center + Math.cos(angle + arc / 2) * turnplate.big_textRadius, center + Math.sin(angle + arc / 2) * turnplate.big_textRadius);
-                    //ctx.rotate(angle + arc / 2 + Math.PI / 2);
-                    ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
-                    ctx.restore();
-                }
+            var text = this.restaraunts[i]['activeName'];
+            //var text = this.restaraunts[i];
+            if(this.restaraunts.length == 6){
+                ctx.translate(center + Math.cos(angle + arc / 2) * this.big_textRadius, center + Math.sin(angle + arc / 2) * this.big_textRadius);
+                ctx.rotate(angle + arc / 2 + Math.PI / 2);
+            }else if(this.restaraunts.length >=6 && this.restaraunts.length <=8) {
+                ctx.translate(center + Math.cos(angle + arc / 1.7) * this.big_textRadius, center + Math.sin(angle + arc / 1.8) * this.big_textRadius);
+                ctx.rotate(angle + arc/2);
+            }else if(this.restaraunts.length >=8 && this.restaraunts.length <=10) {
+                ctx.translate(center + Math.cos(angle + arc / 1.5) * this.big_textRadius, center + Math.sin(angle + arc / 1.8) * this.big_textRadius);
+                ctx.rotate(angle + arc/2);
+            }else {
+                ctx.translate(center + Math.cos(angle + arc / 1.5) * this.big_textRadius, center + Math.sin(angle + arc / 1.8) * this.big_textRadius);
+                ctx.rotate(angle + arc/2);
             }
+            ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
+            ctx.restore();
         }
     }
 }
